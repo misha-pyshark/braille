05bae0f1c526534a8604d7b65116bc47819169f0
Misha <64092567+misha-pyshark@users.noreply.github.com>
2020-11-17 21:49:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -194,6 +194,5 @@ def translation():
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
